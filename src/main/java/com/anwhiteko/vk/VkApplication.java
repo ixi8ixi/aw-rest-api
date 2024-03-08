0d522c2ae997f45b358ed41ae6c8a674fe1aaf06
Belocerkovchenko Andrey <ix8oio8xi@gmail.com>
2024-03-08 16:33:28 +0300

    Add Gradle dependencies and Dockerfile

--- src/main/java/com/anwhiteko/vk/VkApplication.java
+++ src/main/java/com/anwhiteko/vk/VkApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VkApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(VkApplication.class, args);
 	}
