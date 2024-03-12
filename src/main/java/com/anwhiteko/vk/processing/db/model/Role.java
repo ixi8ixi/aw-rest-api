5ecf9c658ed4f437e90b71a288f2372d4d4084b6
Belocerkovchenko Andrey <ix8oio8xi@gmail.com>
2024-03-12 12:54:35 +0300

    Add observer roles and example users for every new role

--- src/main/java/com/anwhiteko/vk/processing/db/model/Role.java
+++ src/main/java/com/anwhiteko/vk/processing/db/model/Role.java
@@ -11,7 +11,11 @@ public enum Role { // todo прочитать как маппятся enumы
     ROLE_ADMIN(EnumSet.allOf(Permission.class)),
     ROLE_POSTS(EnumSet.of(Permission.EDIT_POSTS, Permission.VIEW_POSTS)),
     ROLE_USERS(EnumSet.of(Permission.EDIT_USERS, Permission.VIEW_USERS)),
-    ROLE_ALBUMS(EnumSet.of(Permission.EDIT_ALBUMS, Permission.VIEW_ALBUMS));
+    ROLE_ALBUMS(EnumSet.of(Permission.EDIT_ALBUMS, Permission.VIEW_ALBUMS)),
+    ROLE_TOTAL_OBSERVER(EnumSet.of(Permission.VIEW_POSTS, Permission.VIEW_USERS, Permission.VIEW_ALBUMS)),
+    ROLE_POSTS_OBSERVER(EnumSet.of(Permission.VIEW_POSTS)),
+    ROLE_USERS_OBSERVER(EnumSet.of(Permission.VIEW_USERS)),
+    ROLE_ALBUMS_OBSERVER(EnumSet.of(Permission.VIEW_ALBUMS));
 
     private final Set<Permission> permissions;
 
---
+++ src/main/resources/db/changelog/changes/007-add-example-observers.sql
@@ -0,0 +1,6 @@
+INSERT INTO users(id, username, password, role)
+    VALUES
+        (4, 'echo', '$2a$12$xp7BBHiXwRN50ixunOl.se5WCTVg/1rxpsHB1JzfNPv4zcOAogBVi', 'ROLE_TOTAL_OBSERVER'),
+        (5, 'foxtrot', '$2a$12$3AygqZ1J./czjFH5FRN.HuCgJykBoKoKotHMGrE5S91afXs5CIqKC', 'ROLE_USERS_OBSERVER'),
+        (6, 'golf', '$2a$12$ogxviyhJtn6h2zhmkDO8jesVOpAl0UD.HhDVWa.TCbesQHGrG5cMC', 'ROLE_ALBUMS_OBSERVER'),
+        (7, 'hotel', '$2a$12$CuILnlWeDd8igHtv1r/8YOyg.Sy0z6pNAtgas8uf5NTqrZXCfDu2i', 'ROLE_POSTS_OBSERVER')
